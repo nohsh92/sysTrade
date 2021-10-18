6ac8bfbb78d192011b430e861ae5705f9a97035e
nohsh92 <37391521+nohsh92@users.noreply.github.com>
2021-10-18 23:55:20 +0900

    updated simplified working version

--- 4. simplified(working)/main.py
+++ 4. simplified(working)/main.py
@@ -1,21 +1,17 @@
-import time
-import sys
+import time, sys, os
 from twisted.internet import reactor
 from binance.client import Client # Import the Binance Client
-from binance.websockets import BinanceSocketManager # Import the Binance Socket Manager
+from binance import ThreadedWebsocketManager # Import the Binance Socket Manager
 from datetime import datetime
 from pynput import keyboard
 from config import PUBLIC, SECRET
+from os import path
 
 
 # tutorial @ https://livedataframe.com/live-cryptocurrency-data-python-tutorial/ #
 
-
-# Instantiate a Client
-client = Client(api_key=PUBLIC, api_secret=SECRET)
-
 # Instantiate a BinanceSocketManager, passing in the client that you instantiated
-bm = BinanceSocketManager(client)
+
 
 logFilename = ''
 
@@ -33,19 +29,27 @@ def on_press(key):
 
 def create_file():
     global logFilename
+    print("Creating File")
     dateTimeObj = datetime.now()
     timeStampStr = dateTimeObj.strftime("%y%m%d_%H%M")
-    logFilename = '../BTCUSDTLog' + timeStampStr +'.txt'
+    if(path.isdir('./Logs')):
+        print("Folder exists")
+        logFilename = './Logs/BTCUSDTLog' + timeStampStr +'.txt'
+    else:
+        print("Folder doesn't exist, creating...")
+        os.mkdir('./Logs')
+        logFilename = './Logs/BTCUSDTLog' + timeStampStr +'.txt'
+
     return logFilename
 
 def write_to_file(newTradeData):
     global logFilename
+    # print("Writing to file" + logFilename)
     tradeLog = open(logFilename, 'a')
     tradeLog.write(newTradeData)
     tradeLog.close()
 
 def print_message(msg):
-    # breakpoint()
     # calculate total bitcoins exchanged
     bitcoins_exchanged = float(msg['p']) * float(msg['q']) 
     
@@ -59,13 +63,10 @@ def print_message(msg):
     else:  
         event_side = 'BUY '
        
-    
     #dataAsString = "Time: {} Symbol: {} Price: {} Quantity: {} \n". format(msg['T'], msg['s'], msg['p'], msg['q'])
     dataAsString = "{} - {} - {} - {} - Price: {} - Qty: {} BTC Qty: {}\n". format(timestamp, event_side, msg['t'], msg['s'], float((msg['p'])[0:7]), msg['q'], bitcoins_exchanged)
-    # write_to_file(dataAsString)
     print(dataAsString)
     
-    # breakpoint()
 
 # This is our callback function. For now, it just prints messages as they come.
 def handle_message(msg):
@@ -77,16 +78,8 @@ def handle_message(msg):
     else:
         rawMessage = "{} {} {} {} {} {} {} {}\n". format(msg['E'], msg['T'], msg['t'], msg['a'], msg['b'], ((msg['p'])[0:7]), msg['q'], msg['m'])
         
-        # [NOT USED}Debugging different types #
-        # floatedMessage = "{} {} {} {} {} {} {} {}\n". format(int(msg['E']), int(msg['T']), int(msg['t']), int(msg['a']), int(msg['b']), float((msg['p'])[0:7]), float(msg['q']), bool(msg['m']))
-        # print(rawMessage)
-        # print(floatedMessage)
-        # breakpoint()
-        #############################
-        
         write_to_file(rawMessage)
         print_message(msg)
-    # breakpoint()
     
 def end_program():
     global conn_key
@@ -100,29 +93,25 @@ def end_program():
 
 def run_logger():
     global logFilename, conn_key
-    with keyboard.Listener(on_press=on_press) as listener:
-        while break_program == False:
-            
-            # Start trade socket with 'ETHBTC' and use handle_message to... handle the message.
-            conn_key = bm.start_trade_socket('BTCUSDT', handle_message)
-            # then start the socket manager
-            bm.start()
-            print("starting retrieval:")
-            # let some data flow...
-            # time.sleep(10)
 
-            # end_program()
-        listener.join
+    # Start the socket manager
+    bm.start()
 
-def __main__():
-    global logFilename
+    # Start trade socket with 'ETHBTC' and use handle_message to... handle the message.
+    conn_key = bm.start_trade_socket(handle_message, 'BTCUSDT')
+                
+    print("starting retrieval:")
+
+    bm.join()
+
+
+if __name__ == "__main__":
+    logFilename
     logFilename = create_file()
     startMessage = "Event Time / Trade Time / Trade ID / Seller Order ID / Buyer Order ID / Price / Quantity / Buyer=MarketMaker?\n"
+    
     write_to_file(startMessage)
+
+    bm = ThreadedWebsocketManager(PUBLIC, SECRET)        
+
     run_logger()
-
-
-
-
-__main__()
-
